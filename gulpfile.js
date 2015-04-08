d13fc664ca47067fe9b2bf2bcf823f49f2bd685a
millionhari <mikemhari@gmail.com>
2015-04-08 01:26:52 -0700

    (refactor) Update grunt file to minify css, and build jsx files
    - Restructured application to use dist and src folders

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,32 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
+var notify = require('gulp-notify');
+var minifyCSS = require('gulp-minify-css');
 
-gulp.task('browserify', function() {
-  var b = browserify();
-  b.transform(reactify);
-  b.add('./client/main/stego.js');
-  return b.bundle()
-    .pipe(source('stego.js'))
-    .pipe(gulp.dest('./build'));
+var path = {
+  HTML: './client/main/stego.html',
+  ENTRY_POINT: './client/src/js/App.jsx',
+  OUT: 'bundle.js',
+  PUBLIC: './client/dist/public/js',
+  CSS_SRC: './client/src/css/style.css',
+  CSS_PUBLIC: './client/dist/public/css'
+};
+
+gulp.task('build', function(){
+  browserify({
+    entries: [path.ENTRY_POINT],
+    transform: [reactify]
+  })
+  .bundle()
+  .pipe(source(path.OUT))
+  .pipe(gulp.dest(path.PUBLIC))
+  .pipe(notify('Stego Build Complete!'));
+});
+
+gulp.task('styles', function(){
+  return gulp.src(path.CSS_SRC)
+  .pipe(minifyCSS())
+  .pipe(gulp.dest(path.CSS_PUBLIC))
+  .pipe(notify('Styles Build Complete!'));
 });

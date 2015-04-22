2e542db3db9ead176baed33de11f909b35d2ad6a
millionhari <mikemhari@gmail.com>
2015-04-21 22:49:36 -0700

    (refactor) Move functions of buttons from CommitsBox to SignUp

--- client/src/js/components/CommitsBox.jsx
+++ client/src/js/components/CommitsBox.jsx
@@ -9,11 +9,8 @@ var CommitsBox = React.createClass({
   },
 
   // This function logs in to Github, and triggers a series of AJAX calls that extract the user's info, repos, and commits
-  
+
   // This function logs the user object saved in App.jsx's state
-  pairAccounts: function() {
-    this.props.auth.sendToServer('pairing');
-  },
 
   syncAccount: function() {
     this.props.auth.syncAccount();
@@ -55,8 +52,6 @@ var CommitsBox = React.createClass({
       <div className="commits-box">
         <h2>Commits</h2>
         <Chart parentId="commits-chart" currentValue={this.state.currentValue} max={this.props.max} />
-
-        <a className="button" onClick={this.pairAccounts}>Pair Accounts</a>
         <a className="button" onClick={this.syncAccount}>Sync Account</a>
       </div>
     );
